cd742be193c16fdb8e11327e775aee5a72c65d21
dshehbaj <shehbaj.dhillon@gmail.com>
2021-05-31 19:25:35 -0700

    Added Home container.

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "React";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="Lander">
+        <h1>Notes</h1>
+        <p className="text-muted">A simple note taking app</p>
+      </div>
+    </div>
+  );
+};
