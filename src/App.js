383e36a8c2d3f61291932cd910f5bc54a0c07e1f
dshehbaj <shehbaj.dhillon@gmail.com>
2021-05-31 19:54:09 -0700

    Fixed center bug.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
         <Navbar.Brand className="font-weight-bold text-muted">
-          Scratch
+          Notes
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="Home">
-      <div className="Lander">
+      <div className="lander">
         <h1>Notes</h1>
         <p className="text-muted">A simple note taking app</p>
       </div>
