4f1ac0f9adb743721f799c6399c2f3e5f8414ccc
dshehbaj <shehbaj.dhillon@gmail.com>
2021-06-01 17:03:40 -0700

    Fixed typo.

--- src/libs/contextLib.js
+++ src/libs/contextLib.js
@@ -2,6 +2,6 @@ import { useContext, createContext } from "react";
 
 export const AppContext = createContext(null);
 
-export function useAppConext() {
+export function useAppContext() {
   return useContext(AppContext);
 };
